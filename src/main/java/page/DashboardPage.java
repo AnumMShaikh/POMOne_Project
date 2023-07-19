641fab7b65e9ab9d9bfc66ef2579124d67fce150
AnumMShaikh <135440721+AnumMShaikh@users.noreply.github.com>
2023-07-19 15:53:09 -0600

    pom project

---
+++ src/main/java/page/DashboardPage.java
@@ -0,0 +1,36 @@
+package page;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+public class DashboardPage extends BasePage {
+	WebDriver driver;
+
+	public DashboardPage(WebDriver driver) {
+		this.driver = driver;
+		
+	}
+	
+	@FindBy(how = How.XPATH,using ="//h2[contains(text(),'Dashboard')]") WebElement DASHBOARD_HEADER_ELEMENT;
+	@FindBy(how = How.XPATH,using = "//*[@id=\"side-menu\"]/li[3]/a") WebElement CUSTOMER_MENU_ELEMENT;
+	@FindBy(how = How.XPATH, using = "//*[@id=\"side-menu\"]/li[3]/ul/li[1]/a") WebElement ADD_CUSTOMER_ELEMENT;
+	
+	public void validateDashboardPage(String expectedText) {
+		validateElement(DASHBOARD_HEADER_ELEMENT.getText(),expectedText , "Dashboard page no found");
+	}
+	public void clickCustomerButton() throws InterruptedException {
+		Thread.sleep(2000);
+		CUSTOMER_MENU_ELEMENT.click();
+	}
+	public void clickAddCustomerButton() {
+		ADD_CUSTOMER_ELEMENT.click();
+	}
+	
+		
+		
+		
+		
+	}
+
